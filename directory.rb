b939448a236b63f4b0a244f064093a649921b93e
jdm79 <james.malvern@gmail.com>
2018-08-03 18:12:03 +0100

    added another way to make a default value for a symbol - needs to be tested more though

--- directory.rb
+++ directory.rb
@@ -7,9 +7,10 @@ def input_students
   name = gets.chomp
   puts "Please enter the cohort of the student (if blank, default is November)"
   cohort = gets.chomp
-  if cohort.empty?
-    cohort = "november"
-  end
+  # if cohort.empty?
+  #   cohort = "november"
+  # end
+  cohort.default = "november"
   puts "Please enter the hobby of the student"
   hobby = gets.chomp
   puts "Please enter the height of the student"
@@ -53,26 +54,26 @@ def print_header
   puts "-------------"
 end
 
-# def print(students)
-#   students.each do |student|
-#     if student[:name].length < 12 
-#       puts "#{student[:name]} (#{student[:cohort]} cohort)"
-#     end
-#   end
-# end
-
 def print(students)
-  count = 0
-  while count < students.length 
-      puts "Person #{count+1}. ".center(4) + "#{students[count][:name]}" 
-      puts "cohort: #{students[count][:cohort]}"
-      puts "hobby: #{students[count][:hobby]}"
-      puts "height: #{students[count][:height]}"
-      puts "POB: #{students[count][:POB]}"
-      # puts students[count][:cohort].is_a?(Symbol)
-      count += 1
+  students.each do |student|
+    if student[:cohort] == :november
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
+
+# def print(students)
+#   count = 0
+#   while count < students.length 
+#       puts "Person #{count+1}. ".center(4) + "#{students[count][:name]}" 
+#       puts "cohort: #{students[count][:cohort]}"
+#       puts "hobby: #{students[count][:hobby]}"
+#       puts "height: #{students[count][:height]}"
+#       puts "POB: #{students[count][:POB]}"
+#       # puts students[count][:cohort].is_a?(Symbol)
+#       count += 1
+#   end
+# end
 
 def print_footer(students)
   if students.length == 1
