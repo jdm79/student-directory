0849cd44b1a32048e6d248305c93e319ab41cde8
jdm79 <james.malvern@gmail.com>
2018-08-04 16:52:40 +0100

    added load_students to menu and process, tested successfully

--- lib/directory.rb
+++ lib/directory.rb
@@ -13,6 +13,7 @@ def print_menu
   puts "1. Input the @students"
   puts "2. Show the @students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items  
 end
 
@@ -30,6 +31,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
