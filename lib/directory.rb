bc6f809c50e160ec1ea6cd0b71db60b4efa9d703
jdm79 <james.malvern@gmail.com>
2018-08-04 15:42:44 +0100

    refactored the methods using an instance variable for the students array rather than pass as an argument everywhere

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,8 +1,43 @@
+# declaring an instance variable which can be accessed by all methods
+@students = []
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the @students"
+  puts "2. Show the @students"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
 def input_students
+  puts "To finish, just hit return twice after the last question"
   puts "Please enter the name of the student"
-  puts "To finish, just hit return twice"
   # create an empty array
-  students = []
   # get the first name
   name = gets.chop
   puts "Please enter the cohort of the student (if blank, default is November)"
@@ -21,11 +56,11 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, hobby: hobby, height: height, POB: place_of_birth}
-    if students.length == 1
-      puts "We now have #{students.count} student"
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby, height: height, POB: place_of_birth}
+    if @students.length == 1
+      puts "We now have #{@students.count} student"
     else
-      puts "We now have #{students.count} students"
+      puts "We now have #{@students.count} @students"
     end
     # get another name from the user
     puts "Please enter the name of the student"
@@ -45,45 +80,43 @@ def input_students
     puts "Please enter the place of birth of the student"
     place_of_birth = gets.chomp
   end
-  # return the array of students
-  students
+  # return the array of @students
+  @students
 end
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The @students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print
+  @students.each do |student|
     if student[:cohort] == :november
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
 
-# def print(students)
+# def print(@students)
 #   count = 0
-#   while count < students.length 
-#       puts "Person #{count+1}. ".center(4) + "#{students[count][:name]}" 
-#       puts "cohort: #{students[count][:cohort]}"
-#       puts "hobby: #{students[count][:hobby]}"
-#       puts "height: #{students[count][:height]}"
-#       puts "POB: #{students[count][:POB]}"
-#       # puts students[count][:cohort].is_a?(Symbol)
+#   while count < @students.length 
+#       puts "Person #{count+1}. ".center(4) + "#{@students[count][:name]}" 
+#       puts "cohort: #{@students[count][:cohort]}"
+#       puts "hobby: #{@students[count][:hobby]}"
+#       puts "height: #{@students[count][:height]}"
+#       puts "POB: #{@students[count][:POB]}"
+#       # puts @students[count][:cohort].is_a?(Symbol)
 #       count += 1
 #   end
 # end
 
-def print_footer(students)
-  if students.length == 1
-    puts "Overall, we have #{students.length} great student"
+def print_footer
+  if @students.length == 1
+    puts "Overall, we have #{@students.length} great student"
   else
-    puts "Overall, we have #{students.length} great students"
+    puts "Overall, we have #{@students.length} great @students"
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
+
